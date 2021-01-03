0c09ae468c99132945094d114d3b1960b5c83c72
karthi810 <karthi810@gmail.com>
2021-01-03 15:26:05 -0500

    Selenium hackathon added tc 29

---
+++ src/test/java/com/test/selenium_hackathon/TC29.java
@@ -0,0 +1,67 @@
+package com.test.selenium_hackathon;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Sleeper;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TC29 extends SalesforceUltility {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		String URL = "https://login.salesforce.com/";
+		String usernameVal = "karthika@betsy.com";
+		String passwordVal = "*****";
+
+		LaunchChromeBrowser();
+		gotoURL(URL);
+
+		WebElement username = setXpath("//input[@id='username']");
+		WebElement password = setXpath("//input[@id='password']");
+		WebElement loginButton = setXpath("//input[@id='Login']");
+
+		loginDetails(username, usernameVal);
+		explicitWait(5000, username);
+
+		loginDetails(password, passwordVal);
+		explicitWait(5000, password);
+
+		login(loginButton);
+		WebElement contactsTab = setXpath("//a[normalize-space()='Contacts']");
+		explicitWait(5, contactsTab);
+
+		contactsTab.click();
+		//Thread.sleep(3000);
+		try {
+			explicitWait(2000, driver.findElement(By.xpath("//a[@id='tryLexDialogX']")));
+			WebElement closeDialog = setXpath("//a[@id='tryLexDialogX']");			
+			closeDialog.click();
+			System.out.println("Close Dialog found");
+		} catch (Exception e) {
+			System.out.println("Close Dialog not found");
+		}
+
+		//explicitWait(5, driver.findElement(By.xpath("//select[@id='fcf']")));
+		
+		WebElement recentContact = setXpath("//a[contains(text(),'ABCD')]");
+		recentContact.click();
+		
+		WebElement editButton = setXpath("//td[@id='topButtonRow']//input[@title='Edit']");
+		explicitWait(5000, editButton);
+				
+		
+		quitBrowser();
+	}
+
+}
